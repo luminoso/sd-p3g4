7de741c8bef168bb6949b694f06cc0eaafb7a24e
Eduardo Sousa <eduardosousa@ua.pt>
2016-03-15 12:00:45 +0000

    Added constants file. Added GameScore and TrialScore enums in RefereeSite. Changed the checkForGameWinner() and checkForMatchWinner().

---
+++ RopeGame/src/RopeGame/Constants.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RopeGame;
+
+/**
+ *
+ * @author ed1000
+ */
+public class Constants {
+    // Game information
+    public static int NUMBER_OF_TRIALS = 6;
+    public static int NUMBER_OF_GAMES = 3;
+    
+    public static int NUMBER_OF_PLAYERS_IN_THE_BENCH = 5;
+    public static int NUMBER_OF_PLAYERS_AT_PLAYGROUND = 3;
+    
+    public static int INITIAL_MINIMUM_FORCE = 10;
+    public static int INITIAL_MAXIMUM_FORCE = 20;
+}
