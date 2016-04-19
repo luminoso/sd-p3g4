168ce7f5f6de9ae118b8e1dba5b643960f460731
Guilherme Cardoso <gjc@ua.pt>
2016-04-19 18:01:20 +0100

    Updated structure according to professor

--- RopeGame/src/Others/InterfaceCoach.java
+++ RopeGame/src/Others/InterfaceCoach.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Others;
 
 import ClientSide.Coach.CoachState;
@@ -12,7 +7,7 @@ import ClientSide.Coach.CoachState;
  * @author luminoso
  */
 public interface InterfaceCoach {
-
+    
     /**
      * Get the current Coach state
      *
--- RopeGame/src/ServerSide/ServiceProviderAgent.java
+++ RopeGame/src/ServerSide/ServiceProviderAgent.java
@@ -5,13 +5,22 @@
  */
 package ServerSide;
 
+import ClientSide.Coach;
+import ClientSide.Contestant;
+import ClientSide.Referee;
 import Communication.Message;
+import Others.CoachStrategy;
+import Others.InterfaceCoach;
+import Others.InterfaceContestant;
+import Others.InterfaceReferee;
 
 /**
  *
  * @author luminoso
  */
-public class ServiceProviderAgent extends Thread {
+public class ServiceProviderAgent extends Thread implements InterfaceCoach,
+        InterfaceContestant,
+        InterfaceReferee {
 
     private ServerCom sconi;
     private static int id = 0;
@@ -19,6 +28,11 @@ public class ServiceProviderAgent extends Thread {
     private final PlaygroundInterface pgi;
     private final RefereeSiteInterface rsi;
     private final GeneralInformationRepositoryInterface giri;
+    
+    private final Enum state;
+    private final int strength;
+    private final int team;
+    private final int contestantId;
 
     ServiceProviderAgent(ServerCom sconi,
             ContestantsBenchInterface cbi,
@@ -31,6 +45,12 @@ public class ServiceProviderAgent extends Thread {
         this.pgi = pgi;
         this.rsi = rsi;
         this.giri = giri;
+        
+        this.state = null;
+        this.strength = 0;
+        this.team = 0;
+        this.contestantId = 0;
+        
     }
 
     @Override
@@ -58,6 +78,76 @@ public class ServiceProviderAgent extends Thread {
         sconi.writeObject(outMessage);
         sconi.close();
 
+    }
+
+    @Override
+    public Coach.CoachState getCoachState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getTeam() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public CoachStrategy getStrategy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setState(Coach.CoachState state) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setStrategy(CoachStrategy strategy) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setTeam(int team) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getContestatId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setContestantId(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Contestant.ContestantState getContestantState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setState(Contestant.ContestantState state) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getStrength() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setStrength(int strength) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Referee.RefereeState getRefereeState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setState(Referee.RefereeState state) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
