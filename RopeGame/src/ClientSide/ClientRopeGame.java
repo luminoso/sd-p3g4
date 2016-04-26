297897a82dd2fe4466162f915852bb3648edd221
Eduardo Sousa <eduardosousa@ua.pt>
2016-04-26 16:51:24 +0100

    Added client initialization

--- RopeGame/src/ClientSide/ClientRopeGame.java
+++ RopeGame/src/ClientSide/ClientRopeGame.java
@@ -1,10 +1,9 @@
 package ClientSide;
 
+import Others.CoachStrategy;
 import Others.KeepWinningTeam;
 import Others.MostStrengthStrategy;
 import RopeGame.Constants;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -12,77 +11,37 @@ import java.util.logging.Logger;
  * @author Guilherme Cardoso
  */
 public class ClientRopeGame {
-
-    /**
-     * 
-     */
-    static String serverHostName = "localhost";
-    
-    /**
-     * 
-     */
-    static int serverPortNumb = 4000;
-
     public static void main(String[] args) {
-
-        // estatico para já
-        // inicializar todos os jogadores, arbitro e coaches
-        // Instantiating all active entities
-        Referee rf = new Referee("Arbit", false);
-
-        Coach[] coaches = new Coach[2];
-        coaches[0] = new Coach("Coach: 1", 1, new MostStrengthStrategy(), false);
-        coaches[1] = new Coach("Coach: 2", 2, new KeepWinningTeam(), false);
-
-        Contestant[][] contestants = new Contestant[2][Constants.NUMBER_OF_PLAYERS_IN_THE_BENCH];
-
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < Constants.NUMBER_OF_PLAYERS_IN_THE_BENCH; j++) {
-                contestants[i][j] = new Contestant("Cont: " + (j + 1) + " Team: " + (i + 1), i + 1, j + 1, randomStrength(), true);
-            }
-        }
-
-        // Adding initial information to the General Information Repository
-        informationRepository.addReferee(rf);
-
-        for (int i = 0; i < coaches.length; i++) {
-            informationRepository.addCoach(coaches[i]);
-            for (int j = 0; j < contestants[i].length; j++) {
-                informationRepository.addContestant(contestants[i][j]);
-            }
-        }
-
-        // Print the main header
-        informationRepository.printHeader();
-
-        // Starting simulation
-        for (int i = 0; i < coaches.length; i++) {
-            coaches[i].start();
-
-            for (int j = 0; j < contestants[i].length; j++) {
-                contestants[i][j].start();
-            }
-        }
-
-        rf.start();
-
-        try {
-            // Waiting for simulation to end
-            rf.join();
-        } catch (InterruptedException ex) {
-            Logger.getLogger(ClientRopeGame.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        for (int i = 0; i < coaches.length; i++) {
-            while (coaches[i].isAlive()) {
-                coaches[i].interrupt();
-            }
-
-            for (Contestant contestant : contestants[i]) {
-                while (contestant.isAlive()) {
-                    contestant.interrupt();
+        if(args.length >= 1) {
+            switch(args[0]) {
+                case "RF": {
+                    Referee ref = new Referee("Referee");
+                    ref.start();
+                    break;
+                }
+                case "CH": {
+                    int team = Integer.parseInt(args[1]);                    
+                    CoachStrategy strategy = (Integer.parseInt(args[2]) == 1 ? new KeepWinningTeam() : new MostStrengthStrategy());
+                    Coach coach = new Coach("Coach " + team, team, strategy);
+                    coach.start();
+                    break;
+                }
+                case "CT": {
+                    int team = Integer.parseInt(args[1]);
+                    int id = Integer.parseInt(args[2]);
+                    int strength = randomStrength();
+                    Contestant contestant = new Contestant("Contestant " + team + ":" + id, team, id, strength);
+                    contestant.start();
+                    break;
+                }
+                default: {
+                    System.out.println("");
+                    System.exit(1);
                 }
             }
+        } else {
+            System.out.println("");
+            System.exit(1);
         }
     }
 
